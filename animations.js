d09afd0f26e21aae4ccdcb6c7ef195d23101ce0b
ericduffield <2058974@johnabbottcollege.net>
2022-11-23 21:24:13 -0500

    Added content to github.io site.

---
+++ animations.js
@@ -0,0 +1,38 @@
+function slide() {
+
+    let menu = document.getElementById("menu");
+
+    if (menu.style.top == "0%") {
+        menu.style.top = "-100vh";
+        // document.documentElement.style.overflow = 'scroll';
+    }
+    else {
+        menu.style.top = "0%";
+        // document.documentElement.style.overflow = 'hidden';
+    }
+
+    let hamburger = document.getElementById("hamburger");
+
+    if (hamburger.className == "open") {
+        hamburger.className = "";
+    }
+    else {
+        hamburger.className = "open";
+    }
+}
+
+document.addEventListener('keydown', function (event) {
+    if (event.key === "Escape") {
+        if (menu.style.top == "0%") {
+            slide();
+        }
+    }
+});
+
+let menu = document.getElementById("menu");
+
+menu.childNodes.forEach(element => {
+    if (element.className != "socials") {
+        element.addEventListener("click", slide);
+    }
+});
